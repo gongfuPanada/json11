ab74715042eba341dfde51553478e0afc76796bc
Steven Kabbes <stevenkabbes@gmail.com>
2014-11-18 15:57:04 +0100

    Use strtod not atof

--- json11.cpp
+++ json11.cpp
@@ -546,7 +546,7 @@ struct JsonParser {
                 i++;
         }
 
-        return std::atof(str.c_str() + start_pos);
+        return std::strtod(str.c_str() + start_pos, nullptr);
     }
 
     /* expect(str, res)
